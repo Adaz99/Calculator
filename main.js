7f9e31a783b276b87bb59ea4f47331293003d278
Airidas <airidassien64@gmail.com>
2022-11-22 15:41:47 +0000

    numbers can display when clicked and able to clear the screen

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ const numButtons = Array.from(document.querySelectorAll(".number"));
 const clearAll = document.querySelector(".clear");
 const display = document.querySelector(".calc_Display");
 
+// Displays the numbers on calculator using click event
+numButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    display.innerHTML += event.target.innerHTML;
+  });
+  console.log(button);
+});
+
+// Clears text from calculator display
+const reset = () => {
+  display.innerHTML = "";
+};
+clearAll.addEventListener("click", reset);
